6fab168fbd9048b8b562aa6377a0b8c1c31c93c1
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-06-12 16:36:34 +0700

    Squashed commit of the following:
    commit 9a9982aebaec6c3f1a0a2bda8e0c8038abac0b97
    Author: Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
    Date:   Thu Jun 12 15:43:49 2025 +0700
    
        change model
    
    commit bb8c743e6160a47811c80bf68621a0e7951b7f60
    Author: Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
    Date:   Thu Jun 12 15:34:34 2025 +0700
    
        change model
    
    commit 991150cf99369c9b9d06b17a5bcef664ae2a65c5
    Author: Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
    Date:   Thu Jun 12 15:09:24 2025 +0700
    
        delete file write
    
    commit 5680ff9766f0de7a1a506e8a194ac8d6d09c41b5
    Author: Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
    Date:   Thu Jun 12 15:02:02 2025 +0700
    
        API return
    
    commit eb07ffd0e58143e5962afa7b38d64b3441064a5d
    Author: Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
    Date:   Thu Jun 12 14:41:54 2025 +0700
    
        Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
+# fastapi dev main.py
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
+from fastapi import FastAPI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+app = FastAPI()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro-preview-03-25")
+
+class contentFromUser(BaseModel):
+    content_name: str
 
 with open("prompt.txt", "r", encoding="utf-8") as f:
     structure = f.read()
 
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "คุณคือคนที่ต้องสร้างโจทย์ competetive programming โดยต้องทำตามโครงสร้างใน human message อย่างเคร่งครัด"),
-    ("human", "ฉันกำลังจะสอนนักเรียนระดับ สอวน. คอมพิวเตอร์ ให้เขียนโปรแกรมเรื่อง **{content}**\nช่วยสร้างโมดูลการเรียนรู้ที่มีโครงสร้างโปรเจกต์แบบเดียวกับหัวข้อ {content}  โดยมีรายละเอียดดังนี้\n\n:"+structure)
-])
+@app.post("/generate-task")
+async def generate_task(contentFromUser: contentFromUser):
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "คุณคือคนที่ต้องสร้างโจทย์ competetive programming โดยต้องทำตามโครงสร้างใน human message อย่างเคร่งครัด"),
+        ("human", "ฉันกำลังจะสอนนักเรียนระดับ สอวน. คอมพิวเตอร์ ให้เขียนโปรแกรมเรื่อง **{content}**\nช่วยสร้างโมดูลการเรียนรู้ที่มีโครงสร้างโปรเจกต์แบบเดียวกับหัวข้อ {content}  โดยมีรายละเอียดดังนี้\n\n:"+structure)
+    ])
+    chain = prompt|llm
+    res = chain.invoke({"content": contentFromUser.content_name})
 
-chain = prompt|llm
+    readme, makefile, header, main, task = res.content.split("________________________________________")
 
-res = chain.invoke({"content":"binary search"})
-
-with open(f"result/full.txt", "w", encoding="utf-8") as f:
-    f.write(res.content)
-
-txtfile = res.content.split("________________________________________")
-
-fileName = ["readme.md", "Makefile", "include/header.h", "tests/main.cpp", "src/task.cpp"]
-for file, name in zip(txtfile, fileName):
-    with open(f"result/{name}", "w", encoding="utf-8") as f:
-        f.write(file)
-
-print("success")
+    return {"readme.md": readme,
+            "Makefile": makefile,
+            "header.h": header,
+            "main.cpp": main,
+            "task.cpp": task
+            }
